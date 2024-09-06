049e287b341212571431a1e988a1233f814f67cd
kamangir <arash@kamangir.net>
2024-09-05 19:12:59 -0700

    end of experiment

--- blue_objects/logger.py
+++
@@ -1,6 +0,0 @@
-from abcli.logger import get_logger
-from blue_objects import ICON
-
-logger = get_logger(ICON)
-
-
--- blue_objects/tests/test_version.py
+++
@@ -1,7 +0,0 @@
-from blue_objects import VERSION
-
-
-def test_version():
-    assert VERSION
-
-
